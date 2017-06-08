1e4d2eeeea6cefeb4758a1f80e86789b1ba6d22d
OskarGroth <oskar@cindori.org>
2017-06-08 09:08:46 -0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Oskar Groth on 2017-06-08.
+//  Copyright © 2017 Oskar Groth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
